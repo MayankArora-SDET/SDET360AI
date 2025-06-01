fdb669932c931b051509de4c0e17b785124f2dd4
NehaPal2001 <neha.pal@sdettech.com>
2025-06-01 10:26:18 +0530

    docker config

--- springboot-service/src/main/java/com/sdet/sdet360/config/SecurityConfig.java
+++ springboot-service/src/main/java/com/sdet/sdet360/config/SecurityConfig.java
@@ -74,11 +74,17 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "chrome-extension://nnomjikodbfkmliniolghhafheeccabb","http://tenant1.localhost:4200"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:4201", 
+            "http://sdet360-frontend:80",
+            "chrome-extension://nnomjikodbfkmliniolghhafheeccabb",
+            "http://tenant1.localhost:4201"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(List.of("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
